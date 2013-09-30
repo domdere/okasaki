fb9dc8b02c49102e1d562551ff042651dc3657af
Dom De Re <domdere@domdere.com>
2013-09-30 22:56:21 +1000

    Finished off the BinaryTreeSet

--- src/Data/BinaryTree.hs
+++ src/Data/BinaryTree.hs
@@ -3,10 +3,12 @@ module Data.BinaryTree
     ,   insert
     ,   member
     ,   fromList
+    ,   removedFrom
     ) where
 
 -- | A Binary Search Tree, from Chapter 2.2 of "Purely Functional Data Structures"
-
+-- Work is done in Chapters 3 and 4 to look into how to keep Trees balanced
+--
 data BinaryTree a = Leaf | Node a (BinaryTree a) (BinaryTree a) deriving (Show, Eq)
 
 -- | Returns True if the object is in the tree:
@@ -42,11 +44,32 @@ insert x (Node y left right)
 fromList :: (Ord a) => [a] -> BinaryTree a
 fromList = foldl (flip insert) Leaf
 
--- | gets the largest element in the tree..
+-- | Removes a given element if it exists in the tree,
+-- otherwise it returns the same tree.
 --
--- prop> (\x xs -> getMax (fromList (x:xs)) == maximum (x:xs)) (x :: Int) (xs :: [Int])
+-- >>> 2 `removedFrom` fromList [2,1,3]
+-- Node 1 Leaf (Node 3 Leaf Leaf)
 --
-getMax :: BinaryTree a -> Maybe a
-getMax Leaf             = Nothing
-getMax (Node x _ Leaf)  = Just x
-getMax (Node _ _ right) = getMax right
+-- >>> 1 `removedFrom` fromList [2,1,3]
+-- Node 2 Leaf (Node 3 Leaf Leaf)
+--
+-- >>> 3 `removedFrom` fromList [2,1,3]
+-- Node 2 (Node 1 Leaf Leaf) Leaf
+--
+removedFrom :: (Ord a) => a -> BinaryTree a -> BinaryTree a
+removedFrom _ Leaf = Leaf
+removedFrom x (Node x' left right)
+    | x == x'   = left `mergeTrees` right
+    | x < x'    = Node x' (x `removedFrom` left) right
+    | otherwise = Node x' left (x `removedFrom` right)
+
+-- | merges two trees where all the elements in right are greater than
+-- all the elements in the left.
+--
+-- >>> (fromList [2,1,3]) `mergeTrees` (fromList [5,4,6]) :: BinaryTree Int
+-- Node 2 (Node 1 Leaf Leaf) (Node 3 Leaf (Node 5 (Node 4 Leaf Leaf) (Node 6 Leaf Leaf)))
+--
+mergeTrees :: BinaryTree a -> BinaryTree a -> BinaryTree a
+mergeTrees Leaf t = t
+mergeTrees t Leaf = t
+mergeTrees (Node x left' right') right = Node x left' (right' `mergeTrees` right)
--- src/Data/Set.hs
+++ src/Data/Set.hs
@@ -2,6 +2,16 @@ module Data.Set
     ( Set(..)
     ) where
 
+-- $setup
+-- >>> import Data.Set.BinaryTree
+-- >>> import Control.Applicative hiding (empty)
+-- >>> import Test.QuickCheck
+-- >>> newtype TestSet a = TestSet (BinaryTreeSet a) deriving (Show, Eq)
+--
+-- obvously can't use this Arbitrary
+-- instance to test fromList
+-- >>> instance (Arbitrary a, Ord a) => Arbitrary (TestSet a) where arbitrary = (TestSet . fromList) <$> arbitrary
+
 class Set s where
     empty :: s a
     insert :: (Ord a) => a -> s a -> s a
@@ -13,3 +23,14 @@ class Set s where
 -- | No dupes allowed in sets.
 noDupes :: (Set s, Ord a) => a -> s a -> Bool
 noDupes x set = not $ member x $ x `remove` (x `insert` (x `insert` set))
+
+-- | Set functions
+
+-- | creates a set from a list of orderables:
+--
+-- a property is that for all the elements x in the
+-- input list, x is a member of the tree
+-- prop> (\xs -> all (flip member ((fromList xs) :: BinaryTreeSet Int)) xs) (xs :: [Int])
+--
+fromList :: (Set s, Ord a) => [a] -> s a
+fromList = foldl (flip insert) empty
