4060b05649dab38c70289b10434e49e41cc42239
Dom De Re <domdere@domdere.com>
2013-10-02 22:33:48 +1000

    added some helpers

--- src/Data/SearchTree/Binary.hs
+++ src/Data/SearchTree/Binary.hs
@@ -6,9 +6,12 @@ import Data.SearchTree
 
 -- $setup
 -- >>> import Control.Applicative
+-- >>> import Data.List
 -- >>> import Test.QuickCheck
 --
+-- >>> newtype NoDupes a = NoDupes (BinaryTree a) deriving (Show)
 -- >>> instance (Arbitrary a, Ord a) => Arbitrary (BinaryTree a) where arbitrary = fromList <$> arbitrary
+-- >>> instance (Arbitrary a, Ord a) => Arbitrary (NoDupes a) where arbitrary = (NoDupes . fromList . nub) <$> arbitrary
 
 -- | A Binary Search Tree, from Chapter 2.2 of "Purely Functional Data Structures"
 -- Work is done in Chapters 3 and 4 to look into how to keep Trees balanced
@@ -80,6 +83,38 @@ instance SearchTree BinaryTree where
     removedFromTree = removedFrom'
 
 -- Helpers
+
+checkMember :: (Ord a) => BinaryTree a -> a -> Bool
+checkMember t x = not (x `member'` t)
+
+checkMaybeMember :: (Ord a) => (Maybe a, BinaryTree a) -> Bool
+checkMaybeMember (x, t) = maybe True (checkMember t) x
+
+-- | remove the max element from tree and return both the max element
+-- if there is one and the tree with the element removed.
+--
+-- prop> (\(NoDupes t) -> checkMaybeMember (removeMaxElement t)) (t :: NoDupes Int)
+--
+-- >>> removeMaxElement (fromList [2,3,5,7,4,1])
+-- (Just 7,Node 2 (Node 1 Leaf Leaf) (Node 3 Leaf (Node 5 (Node 4 Leaf Leaf) Leaf)))
+--
+-- >>> removeMaxElement Leaf
+-- (Nothing,Leaf)
+--
+removeMaxElement :: (Ord a) => BinaryTree a -> (Maybe a, BinaryTree a)
+removeMaxElement Leaf = (Nothing, Leaf)
+removeMaxElement (Node x Leaf Leaf) = (Just x, Leaf)
+removeMaxElement (Node x left right) = (maxElement, Node x left newRight)
+    where (maxElement, newRight) = removeMaxElement right
+
+-- | remove the min element from tree and return both the min element
+-- if there is one and the tree with the element removed.
+removeMinElement :: (Ord a) => BinaryTree a -> (Maybe a, BinaryTree a)
+removeMinElement Leaf = (Nothing, Leaf)
+removeMinElement (Node x Leaf Leaf) = (Just x, Leaf)
+removeMinElement (Node x left right) = (minElement, Node x newLeft right)
+    where (minElement, newLeft) = removeMinElement left
+
 
 -- | merges two trees where all the elements in right are greater than
 -- all the elements in the left.
