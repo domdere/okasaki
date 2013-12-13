e1861564915564977b5f10383bada9cae5eb44f8
Dom De Re <domdere@domdere.com>
2013-12-13 21:18:21 +1100

    WIP

--- src/Data/SplayHeap.hs
+++ src/Data/SplayHeap.hs
@@ -23,6 +23,8 @@ THE SOFTWARE.
 module Data.SplayHeap where
 
 import Control.Comonad
+import Control.Monad ( liftM2 )
+import Data.Function ( on )
 import Data.Maybe ( fromMaybe )
 
 -- | From Section 5.4 of PFDS:
@@ -35,11 +37,20 @@ import Data.Maybe ( fromMaybe )
 -- Although every operation can be as bad as O(n), every operation runs in
 -- O(log n) amortized time.
 
+-- $setup
+--
+-- >>> import Control.Applicative
+-- >>> import Test.QuickCheck
+--
+-- >>> instance (Arbitrary a, Ord a) => Arbitrary (SplayHeap a) where arbitrary = fromListL <$> arbitrary
+
 -- | The Splay tree data type resembles the Binary search tree data type
 --
 data SplayHeap a =
         Leaf
     |   Node a (SplayHeap a) (SplayHeap a) deriving (Show, Eq)
+
+data NonEmptySplayHeap a = NonEmptySplayHeap a (SplayHeap a) (SplayHeap a) deriving (Show, Eq)
 
 data Direction = SLeft | SRight deriving (Show, Eq)
 
@@ -59,6 +70,9 @@ data SplayHeapZipper a = SplayHeapZipper
 instance Functor SplayHeap where
     fmap _ Leaf         = Leaf
     fmap f (Node x l r) = Node (f x) (f `fmap` l) (f `fmap` r)
+
+instance Functor NonEmptySplayHeap where
+    fmap f (NonEmptySplayHeap x l r) = (NonEmptySplayHeap (f x) `on` fmap f) l r
 
 instance Functor SplayHeap' where
     fmap f (Left' x c) = Left' (f x) (f `fmap` c)
@@ -85,6 +99,37 @@ instance Comonad SplayHeapZipper where
 
 -- Zipper functions
 
+-- | returns True if the crumb is a left crumb
+--
+-- >>> isLeft (Left' 1 Leaf)
+-- True
+--
+-- >>> isLeft (Right' 1 Leaf)
+-- False
+--
+isLeft :: SplayHeap' a -> Bool
+isLeft (Left' {})   = True
+isLeft (Right' {})  = False
+
+-- | returns True if the crumb is a right crumb
+--
+-- >>> isRight (Left' 1 Leaf)
+-- False
+--
+-- >>> isRight (Right' 1 Leaf)
+-- True
+--
+isRight :: SplayHeap' a -> Bool
+isRight  = not . isLeft
+
+-- | Plugs a Heap into a 1-Hole Context to form a larger Heap
+--
+-- >>> (Right' 3 Leaf) <. (Node 6 (Node 4 Leaf Leaf) (Node 7 Leaf (Node 8 Leaf Leaf)))
+-- Node 3 Leaf (Node 6 (Node 4 Leaf Leaf) (Node 7 Leaf (Node 8 Leaf Leaf)))
+--
+-- >>> (Left' 6 (Node 7 Leaf (Node 8 Leaf Leaf))) <. (Node 4 Leaf Leaf)
+-- Node 6 (Node 4 Leaf Leaf) (Node 7 Leaf (Node 8 Leaf Leaf))
+--
 (<.) :: SplayHeap' a -> SplayHeap a -> SplayHeap a
 (Left' x r) <. t   = Node x t r
 (Right' x l) <. t  = Node x l t
@@ -93,11 +138,13 @@ instance Comonad SplayHeapZipper where
 ss <+ t = foldr (<.) t ss
 
 unzip :: SplayHeapZipper a -> SplayHeap a
-unzip (SplayHeapZipper x l r ss) = ss <+ Node x l r
+unzip = liftM2 (<+) getCrumbs getFocusNode
 
 zipTree :: SplayHeap a -> Maybe (SplayHeapZipper a)
-zipTree Leaf            = Nothing
-zipTree (Node x l r)    = Just $ SplayHeapZipper x l r []
+zipTree = performNonEmptyOp zipNonEmptyTree
+
+zipNonEmptyTree :: NonEmptySplayHeap a -> SplayHeapZipper a
+zipNonEmptyTree (NonEmptySplayHeap x l r) = SplayHeapZipper x l r []
 
 zipzipTree :: SplayHeap a -> SplayHeap (SplayHeapZipper a)
 zipzipTree t = case zipTree t of
@@ -128,8 +175,8 @@ searchTreeZipper x z
 
 rotate :: SplayHeapZipper a -> SplayHeapZipper a
 rotate z@(SplayHeapZipper _ _ _ [])                 = z
-rotate (SplayHeapZipper y ll lr (Left' x r:ss))   = SplayHeapZipper y ll (Node x lr r) ss
-rotate (SplayHeapZipper y rl rr (Right' x l:ss))  = SplayHeapZipper y (Node x l rl) rr ss
+rotate (SplayHeapZipper y ll lr (s@(Left' {}):ss))  = SplayHeapZipper y ll (s <. lr) ss
+rotate (SplayHeapZipper y rl rr (s@(Right' {}):ss)) = SplayHeapZipper y (s <. rl) rr ss
 
 findMin' :: SplayHeapZipper a -> a
 findMin' z = case goLeft z of
@@ -146,23 +193,58 @@ deleteMin' z = case (goLeft z, getCrumbs z, getRightTree z) of
 
 -- SplayHeap Methods
 
+-- | Converts a non empty SplayHeap into a plain splay heap type
+toHeap :: NonEmptySplayHeap a -> SplayHeap a
+toHeap (NonEmptySplayHeap x l r) = Node x l r
+
+toNonEmptySplayTree :: SplayHeap a -> Maybe (NonEmptySplayHeap a)
+toNonEmptySplayTree Leaf = Nothing
+toNonEmptySplayTree (Node x l r) = Just $ NonEmptySplayHeap x l r
+
+-- | runs an operation on a non empty splay heap on a general splay heap,
+-- returning `Nothing` when the splay heap is empty
+--
+performNonEmptyOp :: (NonEmptySplayHeap a -> b) -> SplayHeap a -> Maybe b
+performNonEmptyOp f t = do
+    net <- toNonEmptySplayTree t
+    return $ f net
+
 bigger :: (Ord a) => a -> SplayHeap a -> SplayHeap a
 bigger _ Leaf = Leaf
 bigger pivot t@(Node x l r)
     | x <= pivot = bigger pivot r
     | otherwise = case l of
         Leaf            -> t
-        Node y ll lr    -> Node y (bigger pivot ll) (Node x lr r)
+        Node y ll lr    -> if y <= pivot
+            then
+                Node x (bigger pivot lr) r
+            else
+                Node y (bigger pivot ll) (Node x lr r)
 
--- | Exercise 5.4
+-- | Exercise 5.4: goes through a heap and returns the
+-- heap containing elements smaller than the pivot provided,
+-- with rotations happening along the way
+--
+-- >>> smaller 6 $ Node 4 Leaf (Node 8 Leaf Leaf)
+-- Node 4 Leaf Leaf
+--
 smaller :: (Ord a) => a -> SplayHeap a -> SplayHeap a
 smaller _ Leaf = Leaf
 smaller pivot t@(Node x l r)
     | x > pivot = smaller pivot l
     | otherwise = case r of
         Leaf            -> t
-        Node y rl rr    -> Node y (Node x rl l) (smaller pivot rr)
+        Node y rl rr    -> if y > pivot
+            then
+                Node x l (smaller pivot rl)
+            else
+                Node y (Node x rl l) (smaller pivot rr)
 
+-- | adds an element to the tree.
+--
+-- >>> 6 `addedTo` fromListR [4,8]
+-- Node 6 (Node 4 Leaf Leaf) (Node 8 Leaf Leaf)
+--
 addedTo :: (Ord a) => a -> SplayHeap a -> SplayHeap a
 x `addedTo` t = Node x (smaller x t) (bigger x t)
 
@@ -170,3 +252,14 @@ findMin :: SplayHeap a -> Maybe a
 findMin t = case zipTree t of
     Nothing -> Nothing
     Just z  -> Just $ findMin' z
+
+-- | creates a SplayHeap from a list by inserting elements into an empty tree from left to right
+--
+fromListL :: (Ord a) => [a] -> SplayHeap a
+fromListL = foldl (flip addedTo) Leaf
+
+-- | creates a SplayHeap from a list by inserting elements into an empty tree from right to left
+--
+fromListR :: (Ord a) => [a] -> SplayHeap a
+fromListR = foldr addedTo Leaf
+
